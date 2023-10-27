cfb23f741c97d07937886e4fd4b55e11be83f08b
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-27 13:13:34 -0400

    typever

--- main.ts
+++ main.ts
@@ -1,27 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ernest
+ * Created on: Oct 2023
+ * This program changes color depending on distance
 */
 
 // variables
 let distanceToTarget: number = 0
-let neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+const neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() {
+// on button press
+input.onButtonPressed(Button.A, function () {
   distanceToTarget = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
   basic.showNumber(distanceToTarget)
   basic.clearScreen()
+
+    // if distance is less than 10 cm change led to Red
     if ((distanceToTarget) < 10) {
       neoPixelStrip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
+
+    // else change color to green
     else {
-        basic.clearScreen()
-        neoPixelStrip.showColor(neopixel.colors(NeoPixelColors.Green))
+      neoPixelStrip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
     basic.showIcon(IconNames.Happy)
 })
