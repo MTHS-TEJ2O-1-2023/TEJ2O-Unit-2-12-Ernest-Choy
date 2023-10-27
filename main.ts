e44341695211f988ac59c1f83dddd03ea6756209
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-27 12:57:09 -0400

    Typerver

--- main.ts
+++ main.ts
@@ -5,4 +5,23 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToTarget: number = 0
+let neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+  distanceToTarget = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+  basic.showNumber(distanceToTarget)
+  basic.clearScreen()
+    if ((distanceToTarget) < 10) {
+      neoPixelStrip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    else {
+        basic.clearScreen()
+        neoPixelStrip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+    basic.showIcon(IconNames.Happy)
+})
